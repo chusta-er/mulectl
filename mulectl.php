b95cb238c94221e2711222593db63b3d9e28ffbc
chusta-er <chusta.er@gmail.com>
2021-05-31 14:01:36 +0200

    Environment setup for linux HP laptop.

--- mulectl.php
+++ mulectl.php
@@ -4,12 +4,20 @@
     </head>
     <body>
     <!-- ---------------------------------------------------------------------- -->
+
 <?php 
+$read = "amuled process not found!";
 /* Add redirection so we can get stderr. */
 $handle = popen('/bin/ps --no-headers -f -C amuled 2>&1', 'r');
-#echo "'$handle'; " . gettype($handle) . "\n";
-$read = fread($handle, 2096);
-# echo $read;
+
+#echo "Before: " . $read . "<br/>";
+if ( $handle )
+   {
+   $data = fread($handle, 2096);
+   if ( $data ) $read = $data;
+   }
+#echo "Afer: " . $read;
+
 pclose($handle);
 ?>
 	<textarea rows="10" cols="120"><?=$read?></textarea>
